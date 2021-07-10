8fb080052b1f3e29e55cff5b0391fef7410c9ffd
sadi-1010011 <sadi1010011@gmail.com>
2021-07-10 09:09:50 -0900

    changed browserrouter to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './scripts/App.js';
 import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
